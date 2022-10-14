0270d8c169f2a2df402fd322a4084fc81b11360e
Eliel <elielrj@gmail.com>
2022-10-14 16:53:28 -0300

    Inserido Calculadora de valor do pedido.

---
+++ lib/api/calcular_valor_do_pedido/calcular_valor_do_pedido.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:levv4/api/calcular_distancia_do_pedido/calcular_distancia_do_pedido.dart';
+
+import '../../model/bo/pedido/item_do_pedido/item_do_pedido.dart';
+import '../../model/bo/pedido/pedido.dart';
+
+class CalcularValorDoPedido {
+  Pedido pedido;
+
+  CalcularValorDoPedido(this.pedido);
+
+  double calcular() {
+    ///1 - Calcula valor total da distância do pedido
+    ///
+    double distancia = 0.0;
+    for (ItemDoPedido item in pedido.itensDoPedido!) {
+      distancia +=
+          CalcularDistaciaDoPedido.calcularDistancia(itemDoPedido: item);
+    }
+
+    ///2 - Fórmula de custo da entrega:
+    /// 5,00 => valor mín
+    /// 1.6 por Km
+    ///
+    double y = ((1.6 * distancia) + 5);
+
+    ///3 - Recupera os valores de: volume, peso e qtd
+    ///
+    double volume = pedido.volume! / 10;
+    int quantidade = pedido.itensDoPedido!.length;
+    double peso = pedido.peso! / 100;
+
+    ///4 - Calculo do falor final
+    ///levando em consideração as variáveis: volume, qtd e peso
+    ///
+    double valorFinal = y * (volume + quantidade + peso);
+
+    ///5 - Retornar o valor formatado, sem dizima e com precisão de 2 casas decimais
+    ///
+    return double.parse(valorFinal.toStringAsPrecision(2));
+  }
+}
