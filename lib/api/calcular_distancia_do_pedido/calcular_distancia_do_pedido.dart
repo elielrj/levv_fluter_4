2cf9635b8d02090991d88a795c0266a05c53bba5
Eliel <elielrj@gmail.com>
2022-10-14 16:52:42 -0300

    Inserido Calculadora de pedido.

---
+++ lib/api/calcular_distancia_do_pedido/calcular_distancia_do_pedido.dart
@@ -0,0 +1,24 @@
+
+
+import 'package:flutter/material.dart';
+import 'dart:math';
+import 'package:geolocator/geolocator.dart';
+
+
+import 'package:levv4/model/bo/pedido/item_do_pedido/item_do_pedido.dart';
+
+class CalcularDistaciaDoPedido {
+  static double calcularDistancia({required ItemDoPedido itemDoPedido})  {
+
+     ///start
+   double startLatitude = itemDoPedido.coleta!.geolocalizacao!.longitude;
+   double startLongitude = itemDoPedido.coleta!.geolocalizacao!.longitude;
+
+    ///end
+   double endLatitude = itemDoPedido.entrega!.geolocalizacao!.longitude;
+   double endLongitude = itemDoPedido.entrega!.geolocalizacao!.longitude;
+
+    ///distância
+    return Geolocator.distanceBetween(startLatitude, startLongitude, endLatitude, endLongitude);
+  }
+}
