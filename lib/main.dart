410ab0a5229b4e2ec8f21f3eba1eb170a60c7748
Eliel de Souza Fariã <elielrj@gmail.com>
2023-09-26 19:06:15 -0300

    Ajuste de inicialização de firebase em app flutter.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:levv4/view/splash/tela_splash.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp();
 
   runApp(
     const MaterialApp(
